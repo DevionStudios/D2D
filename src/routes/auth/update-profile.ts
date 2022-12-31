a995a244f414c47667b94366fc972cb39fd6b9da
Debajyoti <debajyotidhar111@gmail.com>
2022-12-31 20:22:25 +0530

    Updated backed for multer support

--- src/routes/auth/update-profile.ts
+++ src/routes/auth/update-profile.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 
+import cloudinary from "../../config/cloudinaryConfig";
+import upload from "../../config/multer.filefilter.config";
+
 import { BadRequestError } from "@devion/common";
 import { currentUser } from "../../middlewares/currentuser";
 import { User } from "../../models/User";
@@ -9,10 +12,12 @@ const router = express.Router();
 router.put(
   "/api/users/update",
   currentUser,
+  upload.array("images", 5),
   async (req: Request, res: Response) => {
+    console.log("Raw files:", req.files);
     try {
-      const { username, name, bio, image, coverImage } = req.body;
-
+      const { username, name, bio } = req.body;
+      var imageUrlList: any[] = [];
       const existingUser = await User.findOne({
         email: req.currentUser!.email,
       });
@@ -20,13 +25,26 @@ router.put(
       if (!existingUser) {
         throw new BadRequestError("User not found");
       }
+      // Upload image to cloudinary and create url
+      if (req.files) {
+        const files = req.files as Express.Multer.File[];
+        console.log("files", files);
+        for (var i = 0; i < files.length; i++) {
+          var locaFilePath = files[i].path;
 
+          // Upload the local image to Cloudinary
+          // and get image url as response
+          var result = await cloudinary.uploader.upload(locaFilePath);
+          imageUrlList.push(result.secure_url);
+        }
+      }
+      console.log("imageUrl:", imageUrlList);
       // Update user details if they are different, otherwise fallback to existing values
       existingUser.username = username || existingUser.username;
       existingUser.name = name || existingUser.name;
       existingUser.bio = bio || existingUser.bio;
-      existingUser.image = image || existingUser.image;
-      existingUser.coverImage = coverImage || existingUser.coverImage;
+      existingUser.image = imageUrlList[0] || existingUser.image;
+      existingUser.coverImage = imageUrlList[1] || existingUser.coverImage;
 
       await existingUser.save();
 
