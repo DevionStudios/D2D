202a93abcf598d525397cbb4442213a058518cfa
metal-oopa <sudip5banerjee1974@gmail.com>
2022-12-31 20:01:59 +0530

    fix styling

--- src/pages/account/settings/index.js
+++ src/pages/account/settings/index.js
@@ -1,8 +1,8 @@
 import { AccountPageLayout } from "~/components/Common/Layouts/AccountPageLayout";
 import { Navbar } from "~/components/Common/Navbar";
 
-export default function AccountPage() {
-  return <AccountPageLayout />;
+export default function AccountPage({ currentUser }) {
+  return <AccountPageLayout currentUser={currentUser} />;
 }
 
 AccountPage.getLayout = function getLayout(page) {
