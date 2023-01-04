83a50e14de238fc939b38acfe23e6c832ad6a613
Debajyoti <debajyotidhar111@gmail.com>
2023-01-04 06:22:09 +0530

    Updated fetch user

--- src/routes/auth/fetch-user.ts
+++ src/routes/auth/fetch-user.ts
@@ -8,11 +8,13 @@ const router = express.Router();
 router.get(
   "/api/users/otheruser/:nameUser",
   async (req: Request, res: Response) => {
-    console.log();
+    console.log(req.params.nameUser);
     try {
       const existingUser = await User.findOne({
         username: req.params.nameUser,
-      });
+      })
+        .populate({ path: "followers" })
+        .populate({ path: "following" });
       if (!existingUser) {
         throw new BadRequestError("User not found!");
       }
