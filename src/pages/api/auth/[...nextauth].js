64bd686fa914ab0199f60a06d27352162d167f76
metal-oopa <sudip5banerjee1974@gmail.com>
2023-01-04 22:26:18 +0530

    update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import TwitterProvider from "next-auth/providers/twitter";
 export const authOptions = {
   providers: [
     TwitterProvider({
-      clientId: "dnRaQzRicVVFeEhvYkZqWlVVNnU6MTpjaQ",
-      clientSecret: "lUS7wq04Zja7BjiX6mBhnWkSoNC80ZxYf9aALc75dtaypitcmj",
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
       version: "2.0",
     }),
   ],
