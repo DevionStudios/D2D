b82b15a5b5b55d0a4fc9475607cd0c87c130e8b1
Debajyoti <debajyotidhar111@gmail.com>
2023-01-06 04:13:37 +0530

    Added Ipfs Nft Contract

---
+++ contracts/deploy/02-deploy-IpfsNft.js
@@ -0,0 +1,21 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+module.exports = async ({ getNamedAccounts, deploymets }) => {
+  const { deployer } = await getNamedAccounts();
+  const { deploy, log } = deployments;
+  const args = [];
+  log("----Deploying----");
+  const d2dTokenContract = await deploy("IpfsNft", {
+    from: deployer,
+    log: true,
+    args: args,
+    waitConfirmations: 1,
+  });
+  log("----Deployed----");
+  if (!developmentChains.includes(network.name)) {
+    log("-----Verifying-----");
+    verify(d2dTokenContract.address, args);
+    log("----Verification was Successful----");
+  }
+};
