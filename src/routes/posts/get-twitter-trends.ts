135c1c4a9b729a9d4c143dad2e4c1cfa728dfb5a
metal-oopa <sudip5banerjee1974@gmail.com>
2023-01-05 20:23:36 +0530

    remove auth from  view twitter trends

--- src/routes/posts/get-twitter-trends.ts
+++ src/routes/posts/get-twitter-trends.ts
@@ -10,46 +10,29 @@ import { TwitterApi } from "twitter-api-v2";
 const router = express.Router();
 dotenv.config();
 
-router.get(
-  "/api/tweets/trending",
-  currentUser,
-  async (req: Request, res: Response) => {
-    const { currentUser } = req;
-    if (!currentUser) {
-      throw new BadRequestError("User not found!");
-    }
+router.get("/api/tweets/trending", async (req: Request, res: Response) => {
+  try {
+    const TwitterClient = new TwitterApi({
+      appKey: process.env.TWITTER_API_KEY!,
+      appSecret: process.env.TWITTER_API_SECRET!,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN!,
+      accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+    });
 
-    try {
-      const existingUser = await User.findOne({
-        username: currentUser.username,
-      });
-
-      if (!existingUser) {
-        throw new Error("User not found");
+    const appOnlyClientFromConsumer = await TwitterClient.appLogin();
+    const trendingApiResponse = await appOnlyClientFromConsumer.v2.search(
+      "trending -is:retweet -is:reply -is:quote",
+      {
+        max_results: 90,
+        "tweet.fields": ["created_at"],
       }
+    );
 
-      const TwitterClient = new TwitterApi({
-        appKey: process.env.TWITTER_API_KEY!,
-        appSecret: process.env.TWITTER_API_SECRET!,
-        accessToken: process.env.TWITTER_ACCESS_TOKEN!,
-        accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
-      });
-
-      const appOnlyClientFromConsumer = await TwitterClient.appLogin();
-      const trendingApiResponse = await appOnlyClientFromConsumer.v2.search(
-        "trending -is:retweet -is:reply -is:quote",
-        {
-          max_results: 90,
-          "tweet.fields": ["created_at"],
-        }
-      );
-
-      res.send(trendingApiResponse.data.data);
-    } catch (err) {
-      console.log(err);
-      res.status(400).send({ message: err });
-    }
+    res.send(trendingApiResponse.data.data);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: err });
   }
-);
+});
 
 export { router as getTrendingTweetsRouter };
