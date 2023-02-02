c59d4336151b316ddefa9816b95aa472db2b5b0b
metal-oopa <sudip5banerjee1974@gmail.com>
2023-02-02 18:51:05 +0530

    add new story button in story bar

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { HiOutlinePlus } from "react-icons/hi";
 import { Link } from "../ui/Link";
 
 export const StoryBar = () => {
@@ -26,8 +27,15 @@ export const StoryBar = () => {
 
   return stories && stories.length > 0 ? (
     <>
-      <div className="bg-white/70 dark:bg-black backdrop-blur-md sm:px-4 md:px-6 lg:px-10 xl:px-12 z-10 pb-2 truncate">
+      <div className="bg-white/70 dark:bg-black backdrop-blur-md mx-4 sm:px-4 md:px-6 lg:px-10 xl:px-12 z-10 pb-2 truncate">
         <div className="mx-auto max-w-7xl relative flex justify-start py-1 xl:grid xl:grid-cols-10 overflow-x-scroll scrollbar-hide">
+          <div className="flex-shrink-0">
+            <Link href={`/stories/new`} className="no-underline">
+              <div className="flex-shrink-0 mx-1 ">
+                <HiOutlinePlus className="h-12 w-12 rounded-full ring-2 ring-red-500" />
+              </div>
+            </Link>
+          </div>
           {stories &&
             stories.length > 0 &&
             stories.map((story, index) => {
