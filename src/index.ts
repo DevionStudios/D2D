da8b856efed0d538a97b310293c1982ddc4413d6
Debajyoti <debajyotidhar111@gmail.com>
2023-02-18 21:45:28 +0530

    Fixed socekt

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ const start = async () => {
     });
     const io = socket(server, {
       cors: {
-        origin: "http://localhost:3000",
+        origin: true,
         credentials: true,
       },
     });
