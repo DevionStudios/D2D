29efa921793a37d0fe9faefd8039c052f2dd1977
unknown <barkuntal6@gmail.com>
2023-03-07 02:15:28 +0530

    Rebase

--- foxxi/android/app/src/main/kotlin/com/example/foxxi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.foxxi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- foxxi/lib/constants.dart
+++
@@ -1,2 +0,0 @@
-//const url = 'http://10.0.2.2:5000';
-const url = 'http://localhost:5000';
