af5f6fc173ce4dbf86f71788e85c186e830b3053
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-07-20 23:08:36 +0530

    Added dotenv fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'package:foxxi/providers/navigation_argument_data_provider.dart';
 import 'package:foxxi/providers/post_provider.dart';
 import 'package:foxxi/providers/story_provider.dart';
@@ -9,16 +8,18 @@ import 'package:foxxi/providers/user_provider.dart';
 import 'package:foxxi/providers/wallet_address.dart';
 import 'package:foxxi/router.dart';
 import 'package:foxxi/screens/splash_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: ((context) => WalletAddressProvider())),
       ChangeNotifierProvider(create: ((context) => UserProvider())),
       ChangeNotifierProvider(create: ((context) => PostProvider())),
-      ChangeNotifierProvider(create: ((context) => ThemeProvider( ))),
-      ChangeNotifierProvider(create: (context) => ScreenNavigationArgumentProvider()),
+      ChangeNotifierProvider(create: ((context) => ThemeProvider())),
+      ChangeNotifierProvider(
+          create: (context) => ScreenNavigationArgumentProvider()),
       ChangeNotifierProvider(create: ((context) => StoryProvider())),
     ],
     child: MyApp(),
@@ -26,36 +27,32 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-    late ThemeMode theme ;
-
+  late ThemeMode theme;
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
-    
     readThemeMode();
 
     super.initState();
   }
 
   void readThemeMode() {
-    
     Provider.of<ThemeProvider>(context, listen: false)
         .readSavedThemeInstance()
         .then((value) {
       setState(() {
         widget.theme = value;
-        Provider.of<ThemeProvider>(context, listen: false).setThemeMode = widget.theme;
-
-
+        Provider.of<ThemeProvider>(context, listen: false).setThemeMode =
+            widget.theme;
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
@@ -63,12 +60,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       darkTheme: MyThemes.darkTheme,
       theme: MyThemes.lightTheme,
-
-    
       onGenerateRoute: generateRoute,
       themeMode: themeProvider.themeMode,
-
-      home: SplashScreen(theme:widget.theme),
+      home: SplashScreen(theme: widget.theme),
     );
   }
 }
