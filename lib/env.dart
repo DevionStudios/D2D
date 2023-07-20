3d50c03aa5d716d845a0f05c7042fa4a7dd314e7
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-07-20 23:10:07 +0530

    Added web3Auth .env fetch

--- lib/env.dart
+++ lib/env.dart
@@ -5,5 +5,4 @@ final String url = dotenv.env['FOXXI_URL']!;
 
 const openAIKey = '';
 const newsCatcherApi = '';
-const web3authApi =
-    'BEEIC0Lz9CJDCy3FzjMgzZ6HduXn76CiG3qUxh_Xzqqa8XRIOPMOW_8Eck76GG8_Fpr8cq86C39eSCcwc7__APk';
+final String web3authApi = dotenv.env['WEB3AUTHAPI']!;
