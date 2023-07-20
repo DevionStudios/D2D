b6a75c92da240c1d3b03162911caf764df21ddef
Deepak Rai <103811779+StrawHATDEEPAK@users.noreply.github.com>
2023-07-20 23:08:07 +0530

    Added fetch FOXXI_URL from .env

--- lib/env.dart
+++ lib/env.dart
@@ -1,7 +1,9 @@
-const url = 'https://foxxi.vercel.app';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final String url = dotenv.env['FOXXI_URL']!;
 // const url = 'https://localhost:5000';
 
-const openAIKey = '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';
-const newsCatcherApi = 'WVap9FVtjQzo_999FDIB6vfYhP4B4s1s2F7C3fGEpZg';
+const openAIKey = '';
+const newsCatcherApi = '';
 const web3authApi =
     'BEEIC0Lz9CJDCy3FzjMgzZ6HduXn76CiG3qUxh_Xzqqa8XRIOPMOW_8Eck76GG8_Fpr8cq86C39eSCcwc7__APk';
